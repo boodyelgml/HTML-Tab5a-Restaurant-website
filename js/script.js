711341fe9a7bdb4f43b92fd02a3f4f4cf9c461da
boody elgml <booodyelgml@gmail.com>
2019-08-25 18:54:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function () {
+    
+      // ******************************owl carousel customization***********************************
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        rtl: true,
+        loop: true,
+        nav: true,
+        animateOut: 'fadeOut',
+        autoplay: true,
+        autoplayTimeout: 2300,
+
+
+    });
+    
+      // ******************************slider height***********************************
+
+    $(".owl-carousel").height($(window).height() - (+ $(".navbar-search").height()));
+
+    $(".owl-carousel img").height($(window).height() - ( $(".navbar-search").height()) + (200));
+
+    
+    
+      // ******************************smooth scroll***********************************
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+    
+    
+    
+    // ******************************NAVBAR***********************************
+    $(".navbar-default .navbar-nav > li").on("click" , function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+    
+    
+  
+    
+    
+    
+    
+    
+    
+});
