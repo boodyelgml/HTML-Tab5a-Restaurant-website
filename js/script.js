ea5424a63ef0211136fd168e80febedbac3d1ab9
boody elgml <booodyelgml@gmail.com>
2019-08-27 03:24:16 +0300

    solve design

--- js/script.js
+++ js/script.js
@@ -1,55 +1,37 @@
-
 $(document).ready(function () {
-    
-      // ******************************owl carousel customization***********************************
+    // ******************************owl carousel customization***********************************
     $(".owl-carousel").owlCarousel({
-        items: 1,
-        rtl: true,
-        loop: true,
-        nav: true,
-        animateOut: 'fadeOut',
-        autoplay: true,
-        autoplayTimeout: 2300,
-
-
+        items: 1
+        , rtl: true
+        , loop: true
+        , nav: true
+        , animateOut: 'fadeOut'
+        , autoplay: true
+        , autoplayTimeout: 2300
+    , });
+    // ******************************slider height***********************************
+    
+    $(window).on("resize , load", function () {
+            if ($(window).innerWidth() >= 767) {
+               $(".owl-carousel").height($(window).height() - ($(".navbar-search").height()));
+                $(".owl-carousel img").height($(window).height() - ($(".navbar-search").height()) + (200));
+            }else{
+                  $(".owl-carousel").height(($(window).height() - ($(".navbar-search").height())) / 1.5);
+                $(".owl-carousel img").height(($(window).height() - ($(".navbar-search").height()) + (200)) / 1.5);
+            }
+           
+        })
+        // ******************************smooth scroll***********************************
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        } // End if
     });
-    
-      // ******************************slider height***********************************
-
-    $(".owl-carousel").height($(window).height() - (+ $(".navbar-search").height()));
-
-    $(".owl-carousel img").height($(window).height() - ( $(".navbar-search").height()) + (200));
-
-    
-    
-      // ******************************smooth scroll***********************************
-  $("a").on('click', function(event) {
-
-    if (this.hash !== "") {
-      event.preventDefault();
-
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-    
-    
-    
     // ******************************NAVBAR***********************************
- 
-    
-    
-  
-    
-    
-    
-    
-    
-    
 });
